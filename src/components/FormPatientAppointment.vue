daf327ab28fc0618fa9916e034687177eec439fe
mufrick01 <mrmufrick@gmail.com>
2024-10-17 04:05:33 -0300

    falta agregar logica a formulario

---
+++ src/components/FormPatientAppointment.vue
@@ -0,0 +1,75 @@
+import FormPatientAppointment from '@/components/FormPatientAppointment.vue';
+<template>
+    <form class="d-flex flex-column gap-2 container p-5 justify-content-center" style="font-size: 15px;">
+        <h2 class="text-center mb-4" style="font-size: 50px;" >Add Patient Appointment</h2>
+        <label class="row">
+            <span class="col-4 text-start">Name</span>
+            <input type="text" class="col-8" v-model="formPatientAppointment.name">
+        </label>
+        <label class="row">
+            <span class="col-4 text-start">Date</span>
+            <input type="date" class="col-8" v-model="formPatientAppointment.date">
+        </label>
+        <label class="row">
+            <span class="col-4 text-start">Time</span>
+            <input type="time" class="col-8" v-model="formPatientAppointment.time">
+        </label>
+        <label class="row">
+            <span class="col-4 text-start">Severity</span>
+            <select class="col-8" v-model="formPatientAppointment.severity">
+                <option v-for="( { type, color }, index) in caseSeverity" :key="index" :value="color">
+                    {{ type }}
+                </option>
+            </select>
+        </label>
+        <label class="row">
+            <span class="col-4 text-start">Notes</span>
+            <textarea class="col-8" v-model="formPatientAppointment.notes"></textarea>
+        </label>
+        <button type="submit" class="btn btn-primary col-4 ms-auto">ADD</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'FormPatientAppointment',
+    data() {
+        return {
+            caseSeverity: [
+                {
+                    type: 'Emergent',
+                    color: '#f00'
+                },
+                {
+                    type: 'Urgent',
+                    color: '#ff0'
+                },
+                {
+                    type: 'Non-Urgent',
+                    color: '#f00'
+                }
+            ],
+
+            formPatientAppointment: {
+                name: '',
+                date: '',
+                time: '',
+                severity: '',
+                notes: '',
+            }
+        };
+    },
+    props: {
+        propA: String,
+        propB: {
+            type: String,
+            default: 'default-value',
+            required: true,
+        },
+    }
+};
+</script>
+
+<style scoped>
+/* Estilos aquí */
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-block d-md-flex">
+    <FormPatientAppointment style="width: 400px;" class="h-vh"/>
+    <div class="flex-grow-1 bg-white">
+      asd
+    </div>
+  </div>
+</template>
+
+<script>
+import FormPatientAppointment from '@/components/FormPatientAppointment.vue';
+
+export default {
+  name: 'MainPage',
+  components: {
+    FormPatientAppointment
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+<style scoped>
+.h-vh{
+  height: 100vh;
+}
+</style>
